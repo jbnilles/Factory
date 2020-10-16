bfe9932891908b0dc5368e061e406ebd38769063
jbnilles <jbnilles24@gmail.com>
2020-10-16 16:40:13 -0700

    add nested results to search page/ add js and jquery scripts

--- Factory/wwwroot/js/search.js
+++ Factory/wwwroot/js/search.js
@@ -1,13 +1,29 @@
 $(document).ready(function () {
     
-  $('.toggle').click(function () {
-      
-      let element = $(this).attr('id').split('-');
-      
-      $('#ul-e-' + element[element.length - 1]).toggle();
-      
+  $('.toggle-eng').click(function () {
+    let dom = $('#' + $(this).attr('id'))
+    let element = $(this).attr('id').split('-');
+    if(dom.text() === '+')
+    {
+      dom.text('-')
+    }
+    else{
+      dom.text('+')
+    }
+    $('#ul-e-' + element[element.length - 1]).toggle();
   })
-  
+  $('.toggle-mac').click(function () {
+    let dom = $('#' + $(this).attr('id'))
+    let element = $(this).attr('id').split('-');
+    if(dom.text() === '+')
+    {
+      dom.text('-')
+    }
+    else{
+      dom.text('+')
+    }
+    $('#ul-m-' + element[element.length - 1]).toggle();
+  })
 })
 
 
