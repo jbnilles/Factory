437b57dcc39edf926125c7291c9a1f2d19c1813c
jbnilles <jbnilles24@gmail.com>
2020-10-16 10:14:31 -0700

    add machine controller

--- Sillystringz/Controllers/EngineersController.cs
+++ Sillystringz/Controllers/EngineersController.cs
@@ -16,48 +16,67 @@ namespace Sillystringz.Controllers
         }
         public ActionResult Index()
         {
-        List<Engineer> model = _db.Engines.OrderBy(x => x.Name).ToList();
-        return View(model);
+            List<Engineer> model = _db.Engines.OrderBy(x => x.Name).ToList();
+            return View(model);
         }
         public ActionResult Create()
         {
-        return View();
+            return View();
         }
         [HttpPost]
         public ActionResult Create(Engineer engineer)
         {
-        _db.Engineers.Add(engineer);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+            _db.Engineers.Add(engineer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public ActionResult Details(int id)
         {
-        Engineer model = _db.Engineers.FirstOrDefault(e => e.EngineerId == id);
-        return View(model);
+            Engineer model = _db.Engineers.FirstOrDefault(e => e.EngineerId == id);
+            return View(model);
         }
         public ActionResult Delete(int id)
         {
-        var thisEngineer = _db.Engineers.FirstOrDefault(x => x.EngineerId == id);
-        return View(thisEngineer);
+            Engineer thisEngineer = _db.Engineers.FirstOrDefault(x => x.EngineerId == id);
+            return View(thisEngineer);
         }
 
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
         {
-        var thisEngineer = _db.Engineers.FirstOrDefault(x => x.EngineerId == id);
-        _db.Engineers.Remove(thisEngineer);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+            Engineer thisEngineer = _db.Engineers.FirstOrDefault(x => x.EngineerId == id);
+            _db.Engineers.Remove(thisEngineer);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
         public ActionResult Edit(int id)
         {                    
-            Course thisEngineer = _db.Engineer.FirstOrDefault(x => x.EngineerId == id);
-            return View(thisCourse);
+            Course thisEngineer = _db.Engineers.FirstOrDefault(x => x.EngineerId == id);
+            return View(thisEngineer);
         }
         [HttpPost]
         public ActionResult Edit(Engineer engineer)
         {
             _db.Entry(engineer).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult AddMachine(int id)
+        {
+            Engineer thisEngineer = _db.Engineers.FirstOrDefault(s => s.EngineerId == id);
+            ViewBag.MachineId = new SelectList(_db.Machine, "MachineId", "Name");
+            return View(thisEngineer);
+        }
+        [HttpPost]
+        public ActionResult AddSpecialty(EngineerMachine engineerMachine)
+        {
+            if (engineerMachine.MachineId != 0)
+            {
+                if (_db.EngineerMachine.Where(x => x.EngineerId == engineerMachine.EngineerId && x.MachineId == engineerMachine.MachineId).ToHashSet().Count == 0)
+                {
+                _db.EngineerMachine.Add(engineerMachine);
+                }
+            }
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
--- Sillystringz/Controllers/MachinesController.cs
+++ Sillystringz/Controllers/MachinesController.cs
@@ -9,6 +9,76 @@ namespace Sillystringz.Controllers
 {
     public class MachinesController : Controller
     {
-        
+        private readonly SillystringzContext _db;
+        public MachinesController( SillystringzContext db)
+        {
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            List<Machine> model = _db.Machine.OrderBy(x => x.Name).ToList();
+            return View(model);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Machine machine)
+        {
+            _db.Machine.Add(machine);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            Machine model = _db.Machine.FirstOrDefault(e => e.MachineId == id);
+            return View(model);
+        }
+        public ActionResult Delete(int id)
+        {
+            Engineer thisMachine = _db.Machine.FirstOrDefault(x => x.MachineId == id);
+            return View(thisMachine);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Machine thisMachine = _db.Machine.FirstOrDefault(x => x.MachineId == id);
+            _db.Machine.Remove(thisMachine);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {                    
+            Course thisMachine = _db.Machine.FirstOrDefault(x => x.MachineId == id);
+            return View(thisMachine);
+        }
+        [HttpPost]
+        public ActionResult Edit(Machine machine)
+        {
+            _db.Entry(machine).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult AddEngineer(int id)
+        {
+            Machine thisMachine = _db.Machine.FirstOrDefault(s => s.MachineId == id);
+            ViewBag.EngineerId = new SelectList(_db.Engineers, "MachineId", "Name");
+            return View(thisMachine);
+        }
+        [HttpPost]
+        public ActionResult AddSpecialty(EngineerMachine engineerMachine)
+        {
+            if (engineerMachine.EngineerId != 0)
+            {
+                if (_db.EngineerMachine.Where(x => x.EngineerId == engineerMachine.EngineerId && x.MachineId == engineerMachine.MachineId).ToHashSet().Count == 0)
+                {
+                _db.EngineerMachine.Add(engineerMachine);
+                }
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
