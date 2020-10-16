6bcf951440e1f46f6df6a7764f36a659f672a679
jbnilles <jbnilles24@gmail.com>
2020-10-16 09:55:30 -0700

    add models

--- Sillystringz/Models/DesignTimeDbContextFactory.cs
+++ Sillystringz/Models/DesignTimeDbContextFactory.cs
@@ -1,7 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 namespace Sillystringz.Models
 {
-    public class DesignTimeDbContextFactory
+  public class DesignTimeDbContextFactory
+  {
+    public class SillystringzContextFactory : IDesignTimeDbContextFactory<SillystringzContext>
     {
-        
+
+      SillystringzContext IDesignTimeDbContextFactory<SillystringzContext>.CreateDbContext(string[] args)
+      {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        var builder = new DbContextOptionsBuilder<SillystringzContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        builder.UseMySql(connectionString);
+
+        return new SillystringzContext(builder.Options);
+      }
     }
+  }
 }
--- Sillystringz/Models/Machine.cs
+++ Sillystringz/Models/Machine.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
 namespace Sillystringz.Models
 {
     public class Machine
     {
-        
+        public Machine()
+        {
+            this.EngineerMachines = new HashSet<EngineerMachine>();
+        }
+        public int MachineId {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public virtual ICollection<EngineerMachine> EngineerMachines{get; set;}
     }
 }
